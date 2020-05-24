4ecf7be2f47fa70cb82892077dfd1525f970dbde
Karina Konopleva <konopleva2001karina@gmail.com>
2020-05-24 23:28:27 +0300

    fixed

--- game.py
+++ game.py
@@ -13,12 +13,98 @@ class Display:
 
 class Cactus:
     cactus_height = 70
-    cactus_options = [69, 449, 37, 410, 40, 420]
+    cactus_size = [(69, 449), (37, 410), (40, 420)]
+
+    @staticmethod
+    def create_cactus_arr():
+        array = []
+        for i in 20, 300, 600:
+            choice = random.randrange(0, 3)
+            img = Image.cactus_img[choice]
+            width, height = Cactus.cactus_size[choice]
+            array.append(Object(Display.display_width + i, height, width, img, 4))
+        return array
 
 
 class Player:
     def __init__(self):
         self.health = 3
+        self.make_jump = False
+        self.jump_counter = 30
+        self.max_above = 0
+        self.above_cactus = False
+        self.run_count = 0
+        self.img_counter = 0
+
+    def draw_dino(self):
+        if self.img_counter == 25:
+            self.img_counter = 0
+
+        display.blit(Image.dino_img[self.img_counter // 5], (Player.player_x, Player.player_y))
+        self.img_counter += 1
+
+    def check_health(self):
+        self.health -= 1
+        if self.health == 0:
+            pygame.mixer_music.stop()
+            pygame.mixer.Sound.play(Musician.game_over_sound)
+            return False
+        else:
+            return True
+
+    def show_health(self):
+        show = 0
+        x = 20
+        while show != self.health:
+            display.blit(Image.health_img, (x, 20))
+            x += 40
+            show += 1
+
+    def check_collision(self, barriers):
+        def check_res(player, barrier, barriers):
+            if player.check_health:
+                radius = find_radius(barriers)
+                barrier.set_to_object(radius)
+                return False
+            return True
+
+        for barrier in barriers:
+            if barrier.y == 449:  # it is a little cactus
+                if not self.make_jump:
+                    if barrier.x <= Player.player_x + Player.player_width - 35 <= barrier.x + barrier.width:
+                        if self.check_health:
+                            return check_res(self, barrier, barriers)
+
+                elif self.jump_counter >= 0:
+                    if Player.player_y + Player.player_heigth - 5 >= barrier.y:
+                        if barrier.x <= Player.player_x + Player.player_width - 40 <= barrier.x + barrier.width:
+                            return check_res(self, barrier, barriers)
+
+                else:
+                    if Player.player_y + Player.player_heigth - 10 >= barrier.y:
+                        if barrier.x <= Player.player_x <= barrier.x + barrier.width:
+                            return check_res(self, barrier, barriers)
+
+            else:
+                if not self.make_jump:
+                    if barrier.x <= Player.player_x + Player.player_width - 5 <= barrier.x + barrier.width:
+                        return check_res(self, barrier, barriers)
+
+                    elif self.jump_counter == 10:
+                        if Player.player_y + Player.player_heigth - 5 >= barrier.y:
+                            if barrier.x <= Player.player_x + Player.player_width - 5 <= barrier.x + barrier.width:
+                                return check_res(self, barrier, barriers)
+
+                    elif self.jump_counter >= -1:
+                        if Player.player_y + Player.player_heigth - 5 >= barrier.y:
+                            if barrier.x <= Player.player_x + Player.player_width - 35 <= barrier.x + barrier.width:
+                                return check_res(self, barrier, barriers)
+
+                        else:
+                            if Player.player_y + Player.player_heigth - 10 >= barrier.y:
+                                if barrier.x <= Player.player_x + 5 <= barrier.x + barrier.width:
+                                    return check_res(self, barrier, barriers)
+        return False
 
     player_width = 60
     player_heigth = 100
@@ -26,13 +112,19 @@ class Player:
     player_y = Display.display_height - player_heigth - 100
     clock = pygame.time.Clock()
 
+    def jump(self):
+        if self.jump_counter >= -30:
+            if self.jump_counter == 30:
+                pygame.mixer.Sound.play(Musician.jump_sound)
+
+            Player.player_y -= self.jump_counter / 2.5
+            self.jump_counter -= 1
+        else:
+            self.jump_counter = 30
+            self.make_jump = False
+
 
 class Musician:
-    @staticmethod
-    def load_backgroung_song(name):
-        pygame.mixer_music.load('textures/{}.wav'.format(name))
-        pygame.mixer_music.set_volume(0.3)
-
     background_sound = pygame.mixer.Sound('textures/background.wav')
     jump_sound = pygame.mixer.Sound('textures/jump.wav')
     game_over_sound = pygame.mixer.Sound('textures/game_over.wav')
@@ -43,20 +135,29 @@ class Musician:
     jump_sound.set_volume(0.15)
     game_over_sound.set_volume(0.2)
 
+    @staticmethod
+    def load_backgroung_song(name):
+        pygame.mixer_music.load('textures/{}.wav'.format(name))
+        pygame.mixer_music.set_volume(0.3)
+
 
 class Button:
-    def __init__(self, width, height):
+    def __init__(self, width, height, x, y, message, font_size):
         self.width = width
         self.height = height
         self.inactive_color = (13, 162, 50)
         self.active_color = (23, 204, 50)
+        self.x = x
+        self.message = message
+        self.font_size = font_size
+        self.y = y
 
-    def draw(self, x, y, message, action = None, font_size = 30):
+    def draw(self, action=None):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
-        if x < mouse[0] < x + self.width and y < mouse[1] < y + self.height:
-            pygame.draw.rect(display, self.active_color, (x,  y, self.width, self.height))
+        if self.x < mouse[0] < self.x + self.width and self.y < mouse[1] < self.y + self.height:
+            pygame.draw.rect(display, self.active_color, (self.x, self.y, self.width, self.height))
 
             if click[0] == 1:
                 pygame.mixer.Sound.play(Musician.button_sound)
@@ -68,9 +169,9 @@ class Button:
                     else:
                         action()
         else:
-            pygame.draw.rect(display, self.inactive_color, (x, y, self.width, self.height))
+            pygame.draw.rect(display, self.inactive_color, (self.x, self.y, self.width, self.height))
 
-        print_text(message=message, x=x + 25, y=y - 5, font_size=font_size)
+        print_text(message=self.message, x=self.x + 25, y=self.y - 5, font_size=self.font_size)
 
 
 class Object:
@@ -83,7 +184,6 @@ class Object:
 
     def move(self):
         if self.x >= -self.width:
-            display.blit(self.image, (self.x, self.y))
             self.x -= self.speed
             return True
         else:
@@ -96,22 +196,42 @@ class Object:
         self.image = image
         display.blit(self.image, (self.x, self.y))
 
+    def set_to_object(self, radius):
+        choice = random.randrange(0, 3)
+        img = Image.cactus_img[choice]
+        width, height = Cactus.cactus_size[choice]
+        self.return_self(radius, height, width, img)
+
+    @staticmethod
+    def open_random_object():
+        choice = random.randrange(0, 2)
+        img_of_stone = Image.stone_img[choice]
+
+        choice = random.randrange(0, 2)
+        img_of_cloud = Image.cloud_img[choice]
+
+        stone = Object(Display.display_width, Display.display_height - 80, 10, img_of_stone, 4)
+        cloud = Object(Display.display_width, 80, 70, img_of_cloud, 2)
+
+        return stone, cloud
+
+
+class Health(Object):
+    def __init__(self, x, y, width, image, speed):
+        super().__init__(x, y, width, image, speed)
+        self.hearts = 0
+
 
 class Image:
     stone_img = [pygame.image.load("textures/Objects/Stone{}.png".format(i)) for i in range(2)]
     cloud_img = [pygame.image.load("textures/Objects/Cloud{}.png".format(i)) for i in range(2)]
     cactus_img = [pygame.image.load("textures/Objects/Cactus{}.png".format(i)) for i in range(3)]
     health_img = pygame.image.load('textures/Effects/heart.png')
+    background_img = pygame.image.load('textures/Backgrounds/bg (1).png')
+    land_img = pygame.image.load('textures/Backgrounds/Land.jpg')
+
+    dino_img = [pygame.image.load('textures/Dino/Dino{}.png'.format(i)) for i in range(5)]
     health_img = pygame.transform.scale(health_img, (30, 30))
-
-
-class Settings:
-        make_jump = False
-        jump_counter = 30
-        max_above = 0
-        above_cactus = False
-        run_count = 0
-        img_counter = 0
 
 
 class Scores:
@@ -120,21 +240,22 @@ class Scores:
 
 
 def start_game():
+    player = Player()
     Musician.load_backgroung_song("background")
-    while game_cycle():
+    while game_cycle(player):
         Scores.scores = 0
-        Settings.make_jump = False
-        Settings.jump_counter = 30
+        player.make_jump = False
+        player.jump_counter = 30
         Player.clock = pygame.time.Clock()
-        Settings.health = 2
-        Settings.run_count = 1
+        player.health = 2
+        player.run_count = 1
 
 
 def show_menu():
-    background = pygame.image.load('textures/Backgrounds/bg (1).png')
+    background = Image.background_img
 
-    start_button = Button(270, 70)
-    quit_button = Button(270, 70)
+    start_button = Button(270, 70, 270, 200, 'start game', 60)
+    quit_button = Button(270, 70, 270, 300, '        exit', 50)
 
     show = True
     while show:
@@ -144,27 +265,25 @@ def show_menu():
                 quit()
 
         display.blit(background, (0, 0))
-        start_button.draw(270, 200, 'start game', start_game, 60)
-        quit_button.draw(270, 300, '        exit', quit, 50)
+        start_button.draw(start_game)
+        quit_button.draw(quit)
 
         pygame.display.update()
         Player.clock.tick(60)
 
 
-def game_cycle():
-    player = Player()
+def game_cycle(player):
     pygame.mixer_music.play(-1)
     game = True
-    cactus_arr = []
-    create_cactus_arr(cactus_arr)
-    land = pygame.image.load('textures/Backgrounds/Land.jpg')
+    cactus_arr = Cactus.create_cactus_arr()
+    land = Image.land_img
 
-    stone, cloud = open_random_object()
-    heart = Object(Display.display_width, 280, 30, Image.health_img, 4)
+    stone, cloud = Object.open_random_object()
+    heart = Health(Display.display_width, 280, 30, Image.health_img, 4)
 
-    if Settings.run_count != 0:
+    if player.run_count != 0:
         pygame.mixer.Sound.play(Musician.magic_sound)
-    Settings.run_count += 1
+    player.run_count += 1
 
     while game:
         for event in pygame.event.get():
@@ -174,54 +293,34 @@ def game_cycle():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
-            Settings.make_jump = True
+            player.make_jump = True
         if keys[pygame.K_ESCAPE]:
             pause()
 
-        if Settings.make_jump:
-            jump()
+        if player.make_jump:
+            player.jump()
 
-        count_scores(cactus_arr)
+        count_scores(cactus_arr, player)
 
         display.blit(land, (0, 0))
         print_text('Scores: ' + str(Scores.scores), 600, 10)
 
         draw_array(cactus_arr)
         move_objects(stone, cloud)
-        draw_dino()
+        player.draw_dino()
 
-        if check_collision(cactus_arr, player):
+        if player.check_collision(cactus_arr):
             game = False
 
         heart.move()
+        display.blit(heart.image, (heart.x, heart.y))
         hearts_plus(heart, player)
-        show_health(player)
+        player.show_health()
 
         pygame.display.update()
         Player.clock.tick(60)
 
     return game_over()
-
-
-def jump():
-    if Settings.jump_counter >= -30:
-        if Settings.jump_counter == 30:
-            pygame.mixer.Sound.play(Musician.jump_sound)
-
-        Player.player_y -= Settings.jump_counter / 2.5
-        Settings.jump_counter -= 1
-    else:
-        Settings.jump_counter = 30
-        Settings.make_jump = False
-
-
-def create_cactus_arr(array):
-    for i in 20, 300, 600:
-        choice = random.randrange(0, 3)
-        img = Image.cactus_img[choice]
-        width = Cactus.cactus_options[choice * 2]
-        height = Cactus.cactus_options[choice * 2 + 1]
-        array.append(Object(Display.display_width + i, height, width, img, 4))
 
 
 def find_radius(array):
@@ -242,53 +341,32 @@ def find_radius(array):
     return radius
 
 
-def open_random_object():
-    choice = random.randrange(0, 2)
-    img_of_stone = Image.stone_img[choice]
-
-    choice = random.randrange(0, 2)
-    img_of_cloud = Image.cloud_img[choice]
-
-    stone = Object(Display.display_width, Display.display_height - 80, 10, img_of_stone, 4)
-    cloud = Object(Display.display_width, 80, 70, img_of_cloud, 2)
-
-    return stone, cloud
-
-
 def draw_array(array):
     for cactus in array:
         check = cactus.move()
+        display.blit(cactus.image, (cactus.x, cactus.y))
         if not check:
-            object_return(array, cactus)
+            radius = find_radius(array)
+            cactus.set_to_object(radius)
 
 
 def move_objects(stone, cloud):
     check = stone.move()
+    display.blit(stone.image, (stone.x, stone.y))
     if not check:
         choice = random.randrange(0, 2)
         img_of_stone = Image.stone_img[choice]
         stone.return_self(Display.display_width, 500 + random.randrange(10, 80), stone.width, img_of_stone)
 
     check = cloud.move()
+    display.blit(cloud.image, (cloud.x, cloud.y))
     if not check:
         choice = random.randrange(0, 2)
         img_of_cloud = Image.cloud_img[choice]
         cloud.return_self(Display.display_width, random.randrange(10, 200), stone.width, img_of_cloud)
 
 
-def draw_dino():
-    dino_img = [pygame.image.load('textures/Dino/Dino0.png'), pygame.image.load('textures/Dino/Dino1.png'),
-                pygame.image.load('textures/Dino/Dino2.png'), pygame.image.load('textures/Dino/Dino3.png'),
-                pygame.image.load('textures/Dino/Dino4.png')]
-
-    if Settings.img_counter == 25:
-        Settings.img_counter = 0
-
-    display.blit(dino_img[Settings.img_counter // 5], (Player.player_x, Player.player_y))
-    Settings.img_counter += 1
-
-
-def print_text(message, x, y, font_color = (0, 0, 0), font_type = 'textures/text_style.ttf', font_size = 40 ):
+def print_text(message, x, y, font_color=(0, 0, 0), font_type='textures/text_style.ttf', font_size=40):
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
     display.blit(text, (x, y))
@@ -316,97 +394,20 @@ def pause():
     pygame.mixer_music.unpause()
 
 
-def object_return(objects, object):
-    radius = find_radius(objects)
-    choice = random.randrange(0, 3)
-    img = Image.cactus_img[choice]
-    width = Cactus.cactus_options[choice * 2]
-    height = Cactus.cactus_options[choice * 2 + 1]
-    object.return_self(radius, height, width, img)
-
-
-def check_collision(barriers, player):
-    for barrier in barriers:
-        if barrier.y == 449:  # it is a little cactus
-            if not Settings.make_jump:
-                if barrier.x <= Player.player_x + Player.player_width - 35 <= barrier.x + barrier.width:
-                    if check_health(player):
-                        object_return(barriers, barrier)
-                        return False
-                    else:
-                        return True
-
-            elif Settings.jump_counter >= 0:
-                if Player.player_y + Player.player_heigth - 5 >= barrier.y:
-                    if barrier.x <= Player.player_x + Player.player_width - 40 <= barrier.x + barrier.width:
-                        if check_health(player):
-                            object_return(barriers, barrier)
-                            return False
-                        else:
-                            return True
-
-            else:
-                if Player.player_y + Player.player_heigth - 10 >= barrier.y:
-                    if barrier.x <= Player.player_x <= barrier.x + barrier.width:
-                        if check_health(player):
-                            object_return(barriers, barrier)
-                            return False
-                        else:
-                            return True
-
-        else:
-            if not Settings.make_jump:
-                if barrier.x <= Player.player_x + Player.player_width - 5 <= barrier.x + barrier.width:
-                    if check_health(player):
-                        object_return(barriers, barrier)
-                        return False
-                    else:
-                        return True
-
-                elif Settings.jump_counter == 10:
-                    if Player.player_y + Player.player_heigth - 5 >= barrier.y:
-                        if barrier.x <= Player.player_x + Player.player_width - 5 <= barrier.x + barrier.width:
-                            if check_health(player):
-                                object_return(barriers, barrier)
-                                return False
-                            else:
-                                return True
-
-                elif Settings.jump_counter >= -1:
-                    if Player.player_y + Player.player_heigth - 5 >= barrier.y:
-                        if barrier.x <= Player.player_x + Player.player_width - 35 <= barrier.x + barrier.width:
-                            if check_health(player):
-                                object_return(barriers, barrier)
-                                return False
-                            else:
-                                return True
-
-                    else:
-                        if Player.player_y + Player.player_heigth - 10 >= barrier.y:
-                            if barrier.x <= Player.player_x + 5 <= barrier.x + barrier.width:
-                                if check_health(player):
-                                    object_return(barriers, barrier)
-                                    return False
-                                else:
-                                    return True
-    return False
-
-
-def count_scores(barriers):
+def count_scores(barriers, player):
     above = 0
 
-    if -20 <= Settings.jump_counter < 25:
+    if -20 <= player.jump_counter < 25:
         for barrier in barriers:
             if Player.player_y + Player.player_heigth - 5 <= barrier.y:
-                if barrier.x <= Player.player_x <= barrier.x + barrier.width:
-                    above += 1
-                elif barrier.x <= Player.player_x + Player.player_width <= barrier.x + barrier.width:
+                if barrier.x <= Player.player_x <= barrier.x + barrier.width \
+                        or barrier.x <= Player.player_x + Player.player_width <= barrier.x + barrier.width:
                     above += 1
 
-        Settings.max_above = max(Settings.max_above, above)
+        player.max_above = max(player.max_above, above)
     else:
-        if Settings.jump_counter == -30:
-            Scores.scores += Settings.max_above
+        if player.jump_counter == -30:
+            Scores.scores += player.max_above
             max_above = 0
 
 
@@ -433,25 +434,6 @@ def game_over():
 
         pygame.display.update()
         Player.clock.tick(15)
-
-
-def show_health(player):
-    show = 0
-    x = 20
-    while show != player.health:
-        display.blit(Image.health_img, (x, 20))
-        x += 40
-        show += 1
-
-
-def check_health(player):
-    player.health -= 1
-    if player.health == 0:
-        pygame.mixer_music.stop()
-        pygame.mixer.Sound.play(Musician.game_over_sound)
-        return False
-    else:
-        return True
 
 
 def hearts_plus(heart, player):
